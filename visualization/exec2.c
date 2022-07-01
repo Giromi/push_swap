bc8c3c21e30c428bd933b2de67332bf77dee3c4f
minsuki2 <giromi@cau.ac.kr>
2022-07-01 10:43:41 +0900

    make almost exec

--- visualization/exec2.c
+++ visualization/exec2.c
@@ -6,7 +6,7 @@
 /*   By: minsuki2 <minsuki2@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 22:59:57 by minsuki2          #+#    #+#             */
-/*   Updated: 2022/06/29 14:49:46 by minsuki2         ###   ########.fr       */
+/*   Updated: 2022/07/01 05:14:01 by minsuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int ft_isminus(int c);
-int ft_issp(int c);
-void full_clean(char **array);
-int	stack_lstadd_back(t_stack **lst, t_stack *new);
-t_stack *stack_lstnew(int num);
+int		ft_isminus(int c);
+int		ft_issp(int c);
+void	full_clean(char **array);
+int		stack_lstadd_back(t_stack **lst, t_stack *new);
+t_stack	*stack_lstnew(int num);
 t_stack	*stack_lstlast(t_stack *lst, int check);
-void	*stack_lstfclean(t_stack **lst);
+void	lst_clean(t_stack **lst);
+void	stack_lstfclean(t_cursor *head);
 char	*ft_strchr_null(const char *s, int c);
 
 size_t	ft_strlcat_known(char *dst, size_t dst_len, char const *src,
@@ -30,8 +31,16 @@ size_t	ft_strlcat_known(char *dst, size_t dst_len, char const *src,
 size_t	ft_strlcpy_known(char *dst, char const *src, size_t len
 		, size_t dstsize);
 char	*ft_strjoin_addsp(char const *s1, char const *s2);
-void ra(t_stack **lst);
-void rra(t_stack **lst);
+void	stack_circle(t_stack **lst);
+void	ra(t_cursor *head);
+void	rra(t_cursor *head);
+void	pb(t_cursor *head);
+void sa(t_cursor *head);
+
+void rb(t_cursor *head);
+void rrb(t_cursor *head);
+void rr(t_cursor *head);
+void rrr(t_cursor *head);
 
 int av_check(char **av, t_stack **input)
 {
@@ -76,13 +85,14 @@ int av_check(char **av, t_stack **input)
 
 int main(int ac, char *av[])
 {
-	t_cursor	head;
+	t_cursor	*head;
 	t_stack		*input;
 	t_stack		*stack_a;
 	t_stack		*stack_b;
 	t_stack		*backup_a;
 	t_stack		*backup_b;
 	t_stack		*last;
+	ssize_t		rd;
 	char		**array;
 	char		*buf;
 	int			i;
@@ -98,28 +108,17 @@ int main(int ac, char *av[])
 	if (av_check(av, &input) == ERROR)
 	{
 		printf("ERROR : wrong input\n");
-		stack_lstfclean(&input);
+		lst_clean(&input);
 		return (ERROR);
 	}
-
-	/* last = stack_lstlast(input, '\0'); */
-	/* last->next = first; */
-	stack_a = input;
-	stack_b = NULL;
+	stack_circle(&input);
+	head = (t_cursor *)malloc(sizeof(t_cursor));
+	head->cur_a = input;
+	head->cur_b = NULL;
 	buf = (char *)malloc(sizeof(char) * 4);
 	while (1)
 	{
-		ft_bzero(buf, sizeof(char) * 4);
-		write(1, "command => ", 10);
-		read(0, buf, 3);
-		if (buf == ft_strnstr(buf, "00", 2))
-			break ;
-		else if (buf == ft_strnstr(buf, "ra", 2))
-			ra(&stack_a);
-		else if (buf == ft_strnstr(buf, "rra", 3))
-			rra(&stack_a);
-		else
-			printf("command error\n");
+		/* head->b = stack_a; */
 		/* write(1, buf, 2); */
 		/* write(1, "\n", 1); */
 
@@ -128,39 +127,54 @@ int main(int ac, char *av[])
 		/* else (ft_strnstr(buf, "ra", 2)) */
 		write(1, "-----------------------", 23);
 		printf("\nstack A\t\tstack B\n");
-		printf("--TOP-------------TOP--\n");
-		backup_a = stack_a;
-		backup_b = stack_b;
+		printf("----------TOP----------\n");
+		stack_a = head->cur_a;
+		stack_b = head->cur_b;
 		while (stack_a || stack_b)
 		{
+			if (stack_a && stack_b)
+				printf("    %d\t\t  %d    \n", stack_a->num, stack_b->num);
+			else if (stack_a && !stack_b)
+				printf("    %d\t\t     \n", stack_a->num);
+			else if (!stack_a && stack_b)
+				printf("     \t\t  %d    \n", stack_b->num);
+			else
+				printf("    \t\t    \n");
 			if (stack_a)
-			{
-				c = stack_a->num + '0';
-				write(1, "   ", 3);
-				write(1, &c, 1);
 				stack_a = stack_a->next;
-			}
-			else
-				write(1, " ", 1);
-			write(1, "\t\t", 1);
 			if (stack_b)
-			{
-				c = stack_b->num + '0';
-				write(1, "   ", 3);
-				write(1, &c, 1);
 				stack_b = stack_b->next;
-			}
-			else
-				write(1, " ", 1);
-			write(1, "\n", 1);
 		}
-		stack_a = backup_a;
-		stack_b = backup_b;
-		printf("--BOT-------------BOT--\n\n");
-	}
+		stack_a = head->cur_a;
+		stack_b = head->cur_b;
+		printf("----------BOT----------\n\n");
 
-	stack_lstfclean(&input);
+		ft_bzero(buf, sizeof(char) * 4);
+		write(1, "command => ", 11);
+		rd = read(0, buf, 4);
+		if (buf == ft_strnstr(buf, "00", 2))
+			break ;
+		else if (buf == ft_strnstr(buf, "ra", 2))
+			ra(head);
+		else if (buf == ft_strnstr(buf, "rb", 2))
+			rb(head);
+		else if (buf == ft_strnstr(buf, "rr", 2))
+			rr(head);
+		else if (buf == ft_strnstr(buf, "rra", 3))
+			rra(head);
+		else if (buf == ft_strnstr(buf, "rrb", 3))
+			rrb(head);
+		else if (buf == ft_strnstr(buf, "rrr", 3))
+			rrr(head);
+		else if (buf == ft_strnstr(buf, "sa", 3))
+			sa(head);
+		else if (buf == ft_strnstr(buf, "pb", 3))
+			pb(head);
+		else
+			printf("noting\n");
+	}
 	free(buf);
+	stack_lstfclean(head);
 	/* system("leaks out --quit"); */
 
 	return (0);
--- visualization/exec_utils.c
+++ visualization/exec_utils.c
@@ -6,7 +6,7 @@
 /*   By: minsuki2 <minsuki2@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 03:16:28 by minsuki2          #+#    #+#             */
-/*   Updated: 2022/06/29 15:00:34 by minsuki2         ###   ########.fr       */
+/*   Updated: 2022/07/01 05:56:33 by minsuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,23 +122,6 @@ char	*ft_strchr_null(const char *s, int c)
 	return ((char *)s + i);
 }
 
-t_stack	*stack_lstlast(t_stack *lst, int check)
-{
-	t_stack	*first;
-
-	if (!lst || (check && lst->num == check))
-		return (NULL);
-	while (lst->next)
-	{
-		if (check && lst->next->num == check)
-			return (NULL);
-		lst = lst->next;
-	}
-	return (lst);
-}
-
-
-
 t_stack *stack_lstnew(int num)
 {
 	t_stack	*new;
@@ -147,8 +130,38 @@ t_stack *stack_lstnew(int num)
 	if (!new)
 		return (NULL);
 	new->num = num;
+	new->idx = 0;
 	new->next = NULL;
+	new->priv = NULL;
 	return (new);
+}
+
+
+t_stack	*stack_lstlast(t_stack *lst, int check)
+{
+	t_stack	*first;
+
+	if (!lst || (check && lst->num == check))
+		return (NULL);
+	if (check && lst->num > check)
+		lst->idx++;
+	while (lst->next)
+	{
+		if (check && lst->next->num == check)
+			return (NULL);
+		if (check && lst->next->num > check)
+			lst->next->idx++;
+		lst = lst->next;
+	}
+	return (lst);
+}
+
+void stack_circle(t_stack **lst)
+{
+	t_stack	*last;
+
+	last = stack_lstlast(*lst, '\0');
+	(*lst)->priv = last;
 }
 
 int	stack_lstadd_back(t_stack **lst, t_stack *new)
@@ -165,6 +178,7 @@ int	stack_lstadd_back(t_stack **lst, t_stack *new)
 		if (!tmp)
 			return (ERROR);
 		tmp->next = new;
+		new->priv = tmp;
 	}
 	return (SUCCESS);
 }
@@ -178,46 +192,143 @@ int	stack_lstadd_front(t_stack **lst, t_stack *new)
 	first = stack_lstnew(new->num);
 	free(new);
 	if (!*lst)
+	{
 		first->next = NULL;
+		first->priv = NULL;
+	}
 	else
 		first->next = *lst;
 	*lst = first;
 	return (SUCCESS);
 }
 
-void	*stack_lstfclean(t_stack **lst)
+
+void lst_clean(t_stack **lst)
 {
 	t_stack	*tmp;
 
-	if (!lst)
-		return (NULL);
+	if (!*lst)
+		return ;
 	while (*lst)
 	{
 		tmp = (*lst);
-		*lst = (*lst)->next;
+		(*lst) = (*lst)->next;
 		free(tmp);
-		/* free((*lst)->content); */
 	}
-	return (NULL);
 }
 
-
-
-void ra(t_stack **lst)
+void	stack_lstfclean(t_cursor *head)
 {
-	t_stack	*last;
-
-	last = *lst;
-	*lst = (*lst)->next;
-	stack_lstadd_back(lst, last);
-	last->next = NULL;
+	if (!head)
+		return ;
+	lst_clean(&head->cur_a);
+	lst_clean(&head->cur_b);
+	if (head)
+		free(head);
 }
 
-void rra(t_stack **lst)
+void stack_cut(t_stack **lst)
 {
-	t_stack	*last;
-
-	last = stack_lstlast(*lst, '\0');
-	stack_lstadd_front(lst, last);
+	if ((*lst))
+	{
+		(*lst)->priv = NULL;
+		(*lst)->next = NULL;
+	}
 }
 
+void sa(t_cursor *head)
+{
+	t_stack	*tmp;
+
+	if (!head->cur_a || !head->cur_a->next)
+		return ;
+	head->cur_a->next->priv = head->cur_a->priv;
+	head->cur_a->priv = head->cur_a->next;
+	head->cur_a->next->next->priv = head->cur_a;
+	head->cur_a->next = head->cur_a->next->next;
+	head->cur_a->priv->next = head->cur_a;
+	head->cur_a = head->cur_a->priv;
+}
+
+void pb(t_cursor *head)
+{
+	t_stack *tmp;
+
+	if (!head->cur_a)
+		return ;
+	tmp = head->cur_a;
+	if (head->cur_a->next)
+	{
+		head->cur_a->next->priv = head->cur_a->priv;
+		head->cur_a = head->cur_a->next;
+	}
+	else
+		head->cur_a = NULL;		// if) head->cur_a empty
+	tmp->next = head->cur_b;
+	tmp->priv = tmp;
+	if (head->cur_b)
+		tmp->priv = head->cur_b->priv;
+	head->cur_b = tmp;
+}
+
+void pa(t_cursor *head)
+{
+	t_stack *tmp;
+
+	head->cur_b->priv->next = head->cur_b->next;
+	head->cur_b->next->priv = head->cur_b->priv;
+	tmp = head->cur_b->next;
+	head->cur_b->next = head->cur_a;
+	head->cur_b->priv = NULL;
+	head->cur_a = head->cur_b;
+	head->cur_b = tmp;
+}
+
+void ra(t_cursor *head)
+{
+	if (!head->cur_a || !head->cur_a->next)
+		return ;
+	head->cur_a->priv->next = head->cur_a;
+	head->cur_a = head->cur_a->next;
+	head->cur_a->priv->next = NULL;
+}
+
+void rb(t_cursor *head)
+{
+	if (!head->cur_b || !head->cur_b->next)
+		return ;
+	head->cur_b->priv->next = head->cur_b;
+	head->cur_b = head->cur_b->next;
+	head->cur_b->priv->next = NULL;
+}
+
+void rra(t_cursor *head)
+{
+	if (!head->cur_a || head->cur_a->priv == head->cur_a)
+		return ;
+	head->cur_a->priv->next = head->cur_a;
+	head->cur_a = head->cur_a->priv;
+	head->cur_a->priv->next = NULL;
+}
+
+
+void rrb(t_cursor *head)
+{
+	if (!head->cur_b || head->cur_b->priv == head->cur_b)
+		return ;
+	head->cur_b->priv->next = head->cur_b;
+	head->cur_b = head->cur_b->priv;
+	head->cur_b->priv->next = NULL;
+}
+
+void rr(t_cursor *head)
+{
+	ra(head);
+	rb(head);
+}
+
+void rrr(t_cursor *head)
+{
+	rra(head);
+	rrb(head);
+}
