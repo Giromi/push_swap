6055fe73d048fcc6cad905472bf0079e9805c6e5
minsuki2 <giromi@cau.ac.kr>
2022-07-21 11:16:00 +0900

    quick_sort

--- mandatory/push_swap.c
+++ mandatory/push_swap.c
@@ -6,14 +6,11 @@
 /*   By: minsuki2 <minsuki2@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 05:56:17 by minsuki2          #+#    #+#             */
-/*   Updated: 2022/07/21 10:33:37 by minsuki2         ###   ########.fr       */
+/*   Updated: 2022/07/21 10:45:29 by minsuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-char	stdio_test(t_cursor *head, char **order);
-int		debug_sort_check(t_cursor *head);
 
 static int	check_part(char **av, int *num, char *flag)
 {
@@ -45,7 +42,7 @@ int av_check(char **av, t_stack **input)
 	while (*(++av))
 	{
 		flag = '\0';
-		while (1)					// "" 들어오면 error 나와야해서**av 조건 사용 안됨
+		while (1)
 		{
 			while (ft_issp(**av))
 				(*av)++;
@@ -67,9 +64,6 @@ int av_check(char **av, t_stack **input)
 
 int main(int ac, char *av[])
 {
-	/* int	examine; */
-	/* int	cnt; */
-	//@@@@@@@@@@@@@@
 	t_cursor	head;
 	t_stack		*input;
 	char		*order;
@@ -87,199 +81,11 @@ int main(int ac, char *av[])
 	stack_headset(&head, input);
 	order = (char *)malloc(sizeof(char));
 	ft_bzero(order, sizeof(char));
-
-//-------------------------------------------------------
-//	for (int i = 0; i < 7; i++)
-//		px(&head, head.cur_a, head.cur_b, NULL);
-//	debug_print(&head, 3);
-//	wall_check_stack(&head, &order, 4, '2');
-//	examine = next_check_nspot(&head, 4, '2');
-//	wall_check_stack(&head, &order, 5, '0');
-//	examine = next_check_nspot(&head, 5, '0');
-//-------------------------------------------------------
-
-
 	flag = 0;
 	if (case_check(&head, &order) == ERROR && head.cnt_a > 5)
 		stack_a_to_b(&head, &order, head.cnt_b, head.cnt_a, &flag);
-//	debug_print(&head, 3);
-//	printf("-------------------\n");
-	/* examine = debug_sort_check(&head); */
-//	stdio_test(&head, &order);
-	/* cnt = order_print(order); */
 	order_print(order);
-	/* printf("-------------------\n"); */
-	/* if (examine == ERROR) */
-		/* printf("Total : %d || Sort : %s\n", cnt, "KO"); */
-	/* else */
-		/* printf("Total : %d || Sort : %s\n", cnt, "OK"); */
 	stack_lstfclean(&head);
 	free(order);
-	return (SUCCESS);
-}
-
-void simple_order_print(char cur_order)
-{
-	if (!cur_order)
-		write(1, "start!", 6);
-	if (cur_order == BIT_PA)
-		write(1, "pa", 2);
-	else if (cur_order == (char)BIT_PB)
-		write(1, "pb", 2);
-	else if (cur_order == BIT_SA)
-		write(1, "sa", 2);
-	else if (cur_order == BIT_SB)
-		write(1, "sb", 2);
-	else if (cur_order == BIT_RA)
-		write(1, "ra", 2);
-	else if (cur_order == BIT_RB)
-		write(1, "rb", 2);
-	else if (cur_order == BIT_RRA)
-		write(1, "rra", 3);
-	else if (cur_order == BIT_RRB)
-		write(1, "rrb", 3);
-	write(1, "\n", 1);
-}
-
-
-void debug_print(t_cursor *head, int choice)
-{
-	t_stack		*stack_a;
-	t_stack		*stack_b;
-
-	write(1, "[COMMAND]\n", 10);
-	/* if (cur_order) */
-		/* simple_order_print(*cur_order); */
-	write(1, "-----------------------", 23);
-	ft_printf("\nstack A\t\tstack B\n");
-	ft_printf("----------TOP----------\n");
-	stack_a = head->cur_a;
-	stack_b = head->cur_b;
-	while (stack_a || stack_b)
-	{
-		if (!choice)
-		{
-			if (stack_a && stack_b)
-				ft_printf("    %d\t\t  %d    \n", stack_a->idx, stack_b->idx);
-			else if (stack_a && !stack_b)
-				ft_printf("    %d\t\t     \n", stack_a->idx);
-			else if (!stack_a && stack_b)
-				ft_printf("      \t\t  %d    \n", stack_b->idx);
-			else
-				ft_printf("    \t\t    \n");
-		}
-		else if (choice == 1)
-		{
-			if (stack_a && stack_b)
-				ft_printf("    %d|'%c'| \t  %d|'%c'|    \n", stack_a->idx, stack_a->spot, stack_b->idx, stack_b->spot);
-			else if (stack_a && !stack_b)
-				ft_printf("    %d|'%c'|\t     \n", stack_a->idx, stack_a->spot);
-			else if (!stack_a && stack_b)
-				ft_printf("    \t\t  %d|'%c'|    \n", stack_b->idx, stack_b->spot);
-			else
-				ft_printf("    \t\t    \n");
-		}
-		else if (choice == 3)
-		{
-			if (stack_a && stack_b)
-				ft_printf("    %d|[%d]| \t  %d|[%d]|    \n", stack_a->idx, stack_a->tmp_idx, stack_b->idx, stack_b->tmp_idx);
-			else if (stack_a && !stack_b)
-				ft_printf("    %d|[%d]|\t     \n", stack_a->idx, stack_a->tmp_idx);
-			else if (!stack_a && stack_b)
-				ft_printf("    \t\t  %d|[%d]|    \n", stack_b->idx, stack_b->tmp_idx);
-			else
-				ft_printf("    \t\t    \n");
-		}
-		else
-		{
-			if (stack_a && stack_b)
-				ft_printf("    %d(%d)\t  %d(%d)    \n", stack_a->num, stack_a->idx, stack_b->num, stack_b->idx);
-			else if (stack_a && !stack_b)
-				ft_printf("    %d(%d)\t     \n", stack_a->num, stack_a->idx);
-			else if (!stack_a && stack_b)
-				ft_printf("    \t\t  %d(%d)    \n", stack_b->num, stack_b->idx);
-			else
-				ft_printf("    \t\t    \n");
-		}
-		if (stack_a)
-			stack_a = stack_a->next;
-		if (stack_b)
-			stack_b = stack_b->next;
-	}
-	stack_a = head->cur_a;
-	stack_b = head->cur_b;
-	ft_printf("----------BOT----------\n");
-	ft_printf("   (%d)\t\t  (%d)  \n\n", head->cnt_a, head->cnt_b);
-}
-
-char stdio_test(t_cursor *head, char **order)
-{
-	char		*buf;
-
-	buf = (char *)malloc(sizeof(char) * 5);
-	while (1)
-	{
-		debug_print(head, 1);
-		ft_bzero(buf, sizeof(char) * 5);
-		write(1, "input => ", 9);
-		read(0, buf, 4);
-		if (ft_strnstr(buf, "00\n", 3))
-			break ;
-		else if (ft_strnstr(buf, "pa\n", 3))
-			px(head, head->cur_b, head->cur_a, order);
-		else if (ft_strnstr(buf, "pb\n", 3))
-			px(head, head->cur_a, head->cur_b, order);
-		else if (ft_strnstr(buf, "sa\n", 3))
-			sx(head, head->cur_a, order);
-		else if (ft_strnstr(buf, "sb\n", 3))
-			sx(head, head->cur_b, order);
-		else if (ft_strnstr(buf, "ra\n", 3))
-			rx(head, head->cur_a, order);
-		else if (ft_strnstr(buf, "rb\n", 3))
-			rx(head, head->cur_b, order);
-		else if (ft_strnstr(buf, "rra\n", 4))
-			rrx(head, head->cur_a, order);
-		else if (ft_strnstr(buf, "rrb\n", 4))
-			rrx(head, head->cur_b, order);
-		else if (ft_strnstr(buf, "ss\n", 3))
-		{
-			sx(head, head->cur_a, order);
-			sx(head, head->cur_b, order);
-		}
-		else if (ft_strnstr(buf, "rr\n", 3))
-		{
-			rx(head, head->cur_a, order);
-			rx(head, head->cur_b, order);
-		}
-		else if (ft_strnstr(buf, "rrr\n", 4))
-		{
-			rrx(head, head->cur_a, order);
-			rrx(head, head->cur_b, order);
-		}
-		else
-		{
-			ft_printf("retry!!!\n");
-			continue ;
-		}
-		if (!order)
-			ft_printf("isproblem\n");
-	}
-	free(buf);
-	return (BIT_ALL);
-}
-
-int debug_sort_check(t_cursor *head)
-{
-	t_stack	*lst;
-
-	if (head->cur_b)
-		return (ERROR);
-	lst = head->cur_a;
-	while (lst->next)
-	{
-		if (lst->idx > lst->next->idx)
-			return (ERROR);
-		lst = lst->next;
-	}
 	return (SUCCESS);
 }
--- mandatory/quick_sort.c
+++ mandatory/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: minsuki2 <minsuki2@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 22:49:31 by minsuki2          #+#    #+#             */
-/*   Updated: 2022/07/21 10:11:29 by minsuki2         ###   ########.fr       */
+/*   Updated: 2022/07/21 11:15:34 by minsuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,100 +20,69 @@ void stack_go_to_top(t_cursor *head, char **order, int spot, int cnt)
 		rrx(head, head->cur_b, order);
 }
 
+void pivot_setting(int *pv1, int *pv2, int offset, int n)
+{
+	*pv1 = n / 3 + offset;
+	*pv2 = 2 * (n / 3) + (n % 3 == 2) + offset;
+	if (n / 3 < 4)
+	{
+		*pv2 = n - 4 + offset;
+		*pv1 = (n - 4) / 2 + ((n - 4) % 2 != 0) + offset;
+	}
+}
+
 void stack_b_to_a(t_cursor *head, char **order, int offset, int n, int *flag)
 {
-	int limit = 4;
 	int pv1;
 	int pv2;
 	t_cnt	count;
 
 	*flag = 1;
 	ft_bzero(&count, sizeof(t_cnt));
-	pv1 = n / 3 + offset;
-	pv2 = 2 * (n / 3) + (n % 3 == 2) + offset;
-	if (n <= limit)
-	{
-		wall_check_stack(head, order, n, '2');
+	if (n <= 4 && !wall_check_stack(head, order, n, '2'))
 		while (n--)
 			px(head, head->cur_b, head->cur_a, order);
-		return ;
-	}
-	else if (n / 3 < limit)
-	{
-		pv2 = n - limit + offset;
-		pv1 = (n - limit) / 2 + ((n - limit) % 2 != 0) + offset;
-	}
-	while (n--)
+	pivot_setting(&pv1, &pv2, offset, n);
+	while (n-- > 0)
 	{
 		if (head->cur_b->idx < pv1 && ++count.rb)
 			rx(head, head->cur_b, order);
-		else if (head->cur_b->idx >= pv1 && ++count.pa)
-		{
-			px(head, head->cur_b, head->cur_a, order);
+		else if ((head->cur_b->idx >= pv1 && ++count.pa)
+			&& px(head, head->cur_b, head->cur_a, order))
 			if (head->cur_a->idx < pv2 && ++count.ra)
 				rx(head, head->cur_a, order);
-		}
 	}
-//	debug_print(head, 1);
 	stack_go_to_top(head, order, '3', count.rb);
-//	debug_print(head, 1);
 	stack_a_to_b(head, order, pv1, count.pa - count.ra, flag);
-//	debug_print(head, 1);
 	stack_go_to_top(head, order, '1', count.ra);
-//	debug_print(head, 1);
 	stack_a_to_b(head, order, offset, count.ra, flag);
-//	debug_print(head, 1);
 	stack_b_to_a(head, order, offset, count.rb, flag);
-//	debug_print(head, 1);
 }
 
 void	stack_a_to_b(t_cursor *head, char **order, int offset, int n, int *flag)
 {
-	int limit = 4;
 	int pv1;
 	int pv2;
 	t_cnt	count;
 
 	ft_bzero(&count, sizeof(t_cnt));
-	pv1 = n / 3 + offset;
-	pv2 = 2 * (n / 3) + (n % 3 == 2) + offset;
-	if (n <= limit)
-	{
-		if (head->cnt_a == 5)
-			case_check(head, order);
-		wall_check_stack(head, order, n, '0');
+	if (n <= 4 && !wall_check_stack(head, order, n, '0'))
 		return ;
-	}
-	else if (n / 3 < limit)
-	{
-		pv2 = n - limit + offset;
-		pv1 = (n - limit) / 2 + ((n - limit) % 2 != 0) + offset;
-	}
-	while (n--)
+	pivot_setting(&pv1, &pv2, offset, n);
+	while (n-- > 0)
 	{
 		if (head->cur_a->idx >= pv2 && ++count.ra)
-		{
 			rx(head, head->cur_a, order);
-		}
-		else if (head->cur_a->idx < pv2 && ++count.pb)
-		{
-			px(head, head->cur_a, head->cur_b, order);
+		else if ((head->cur_a->idx < pv2 && ++count.pb)
+			&& px(head, head->cur_a, head->cur_b, order))
 			if (head->cur_b->idx < pv1 && ++count.rb)
-			{
 				rx(head, head->cur_b, order);
-			}
-		}
 	}
-//	debug_print(head, 1);
 	if (*flag != 0)
 		stack_go_to_top(head, order, '1', count.ra);
 	stack_a_to_b(head, order, pv2, count.ra, flag);
-//	debug_print(head, 1);
 	stack_b_to_a(head, order, pv1, count.pb - count.rb, flag);
-//	debug_print(head, 1);
 	if (offset != 0)
 		stack_go_to_top(head, order, '3', count.rb);
-//	debug_print(head, 1);
 	stack_b_to_a(head, order, offset, count.rb, flag);
-//	debug_print(head, 1);
 }
